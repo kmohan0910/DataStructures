070eab2c5544c8692ee6a1f92d23eaeef97c99e5
Mohan <kmohan0910@gmail.com>
2022-09-20 22:16:25 +0530

    35. Search Insert Position

---
+++ BinarySearch/SearchInsert.java
@@ -0,0 +1,21 @@
+public class SearchInsert {
+    public static void main(String[] args) {
+        int[] arr= { 1,2, 3, 4,5};
+        System.out.println(searchInsert(arr, 5));
+        
+    }
+    public static int searchInsert(int[] nums, int target) {
+        
+        int start= 0 , end= nums.length-1;
+            while(start<= end){
+                int mid = start+ (end- start)/2;
+                if (nums[mid]== target){
+                    return mid;
+                }else if (nums[mid]<target) start=mid+1;
+                else{
+                    end=mid-1;
+                }
+            }
+            return start;
+        }
+}
